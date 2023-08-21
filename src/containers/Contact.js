536bc9a321dea2a527dabade5cdb42a9c3b78912
Louisa Ongenyi <o.louisa@ymail.com>
2023-08-21 15:25:28 +0200

    commencement footer

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -6,14 +6,15 @@ import chatMignon from "../assets/pictures/chatmignon_1.png"
 function Contact() {
   return (
     <div className='contactContainer'>
+        <button className='contactButton'>cliquez ici</button>
       {/* <div className='catContactDiv'><img className='catContactImg' src={chatMignon} /></div>
       <p className='contactText email'>E-mail:<br/>mf.rabin@sfr.fr</p>
       <p className='contactText tel'>Télèphone:<br/>06.81.96.05.98</p>
       <p className='contactText adress'>Adresse:<br/>14 rue de la forge 28310 janville-en-beauce</p>
-      <form className='contactText contactForm'>
+      <div className='contactText contactForm'>
         <h2 className='contactText formTitle'>Formulaire du site</h2>
-        <button type='submit'>cliquez ici</button>
-      </form>
+        <button>cliquez ici</button>
+      </div>
       <p className='contactText contactUsText'>Des questions ?<br/>N’hésitez pas à nous contacter !</p> */}
 
     </div>
--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import "../styles/Footer.css";
 
 function Footer() {
   return (
-    <div>
-      
+    <div className='footerContainer'>
+      <p className='footerText1'>Création et collaboration de :<br/>Louisa  Ongenyi et Aurélie Thiré</p>
+      <p className='footerText2'>Pour rappel, les informations personnelles que vous pourriez fournir ne sont ni vendues ni transmises à des tiers. Les données sont collectées uniquement dans le but de vous recontacter si nécessaire et avec votre consentement.</p>
     </div>
   )
 }
