6c5770c2e339726e5784b8bf77dbacd20343f0fd
Louisa Ongenyi <o.louisa@ymail.com>
2023-08-21 14:32:42 +0200

    changement de patte info

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -6,7 +6,7 @@ import chatMignon from "../assets/pictures/chatmignon_1.png"
 function Contact() {
   return (
     <div className='contactContainer'>
-      <div className='catContactDiv'><img className='catContactImg' src={chatMignon} /></div>
+      {/* <div className='catContactDiv'><img className='catContactImg' src={chatMignon} /></div>
       <p className='contactText email'>E-mail:<br/>mf.rabin@sfr.fr</p>
       <p className='contactText tel'>Télèphone:<br/>06.81.96.05.98</p>
       <p className='contactText adress'>Adresse:<br/>14 rue de la forge 28310 janville-en-beauce</p>
@@ -14,7 +14,7 @@ function Contact() {
         <h2 className='contactText formTitle'>Formulaire du site</h2>
         <button type='submit'>cliquez ici</button>
       </form>
-      <p className='contactText contactUsText'>Des questions ?<br/>N’hésitez pas à nous contacter !</p>
+      <p className='contactText contactUsText'>Des questions ?<br/>N’hésitez pas à nous contacter !</p> */}
 
     </div>
   )
